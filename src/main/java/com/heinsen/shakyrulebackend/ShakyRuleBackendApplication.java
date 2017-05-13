64e91e1adf9ad06731c34f3ba253d7c6866699bb
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2017-05-13 09:28:39 +0200

    Initial project structure and gitignore added.

---
+++ src/main/java/com/heinsen/shakyrulebackend/ShakyRuleBackendApplication.java
@@ -0,0 +1,12 @@
+package com.heinsen.shakyrulebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakyRuleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakyRuleBackendApplication.class, args);
+	}
+}
